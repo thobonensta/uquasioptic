Add the change for plot backend and corrected the estimation of variance using chaospy.

main.py
@@ -1,22 +1,23 @@
+import matplotlib
+for backend in [ 'MACOSX','Qt5Agg', 'TkAgg', 'Agg']:
+    try:
+        matplotlib.use(backend, force=True)
+        import matplotlib.pyplot as plt
+
+        print(f"Using backend: {matplotlib.get_backend()}")
+        break
+    except Exception as e:
+        print(f"Failed to use backend {backend}: {e}")
 import matplotlib.pyplot as plt
 from utils.PlaneWave import SimPlaneWave
 import numpy as np
-from utils.GBQuasiOptic import SimGB, SimGB_multi
-
-# TODO 1) Use SimGB to compare with PlaneWave for one layer MUT with stochastic over epsr and thickness
-# TODO 1) @Adam you can add you planewave code to PlaneWave.py and rename the one here to SimPlaneWave_multi
-# TODO 1) This will allow to compare both approach in the two cases
-# TODO 2) Use SimPlaneWave with stochastic thickness for the middle layer (mid) and on theta so that we can compare
-# TODO 2) to the experimentation. You can use uniform laws for the both.
-# TODO 3) Do the same with SimGB_multi
-# TODO For the plot, it is better to plot your mean and confidence interval over the frequency span
+from utils.GBQuasiOptic import SimGB
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     ##### Frequency span for the simulation #####
     freq_GHz = np.linspace(220, 330, 1001, endpoint=True)
     ##### Parameters for the three layers MUT (mat/mid/mar) - mut = material under test
-    # TODO @Adam you will have to construct the dictionary with the thickness mid needed for the PCE UQ (Gauss points)
     eps_mut = 2. - 0.025j
     thickness_mut = 0.00582
     eps_mid = 21. - 0j
@@ -28,7 +29,6 @@ if __name__ == '__main__':
 ]
     # In the case of only one material its eps_mut and x2 for the thickness
     ##### angle in deg #####
-    # TODO Do a UQ on the angle_deg (Uniform law)
     angle_deg = 0
 
     ##### SIMULATION #####

mainPCE.py
@@ -1,7 +1,14 @@
-import math
 import time
 import matplotlib
-matplotlib.use("macOSX")
+for backend in [ 'MACOSX','Qt5Agg', 'TkAgg', 'Agg']:
+    try:
+        matplotlib.use(backend, force=True)
+        import matplotlib.pyplot as plt
+
+        print(f"Using backend: {matplotlib.get_backend()}")
+        break
+    except Exception as e:
+        print(f"Failed to use backend {backend}: {e}")
 import matplotlib.pyplot as plt
 from utils.PlaneWave import SimPlaneWaveList
 import numpy as np
@@ -228,14 +235,14 @@ if __name__ == '__main__':
     plt.plot(freq_GHz,np.abs(S11_m),'-',color='red',label='PCE')
     plt.plot(freq_GHz,np.abs(m_S11),'--',color='blue', alpha = 0.6,label='MC [{0}]'.format(N_sample))
     plt.plot(freq_GHz,np.abs(lst_S11_tmp),':',color='black', alpha = 0.6,label='Deterministe')
-    plt.fill_between(freq_GHz,np.abs(S11_m-1.96*std_S11),np.abs(S11_m+1.96*std_S11),color='red',alpha=0.3)
+    plt.fill_between(freq_GHz,np.abs(S11_m-1.96*np.sqrt(S11_std)),np.abs(S11_m+1.96*np.sqrt(S11_std)),color='red',alpha=0.3)
     plt.ylabel(r'$|S_{11}|$',fontsize=16)
     plt.xlabel('f (GHz)',fontsize=16)
     plt.legend()
     plt.figure()
     plt.plot(freq_GHz,np.abs(S12_m),'-',color='red',label='PW - PCE')
     plt.plot(freq_GHz,np.abs(m_S12),'--',color='blue', alpha = 0.6,label='PW - MC [{0}]'.format(N_sample))
-    plt.fill_between(freq_GHz,np.abs(S12_m-1.96*std_S12),np.abs(S12_m+1.96*std_S12),color='red',alpha=0.3)
+    plt.fill_between(freq_GHz,np.abs(S12_m-1.96*np.sqrt(S12_std)),np.abs(S12_m+1.96*np.sqrt(S12_std)),color='red',alpha=0.3)
     plt.plot(freq_GHz,np.abs(lst_S12_tmp),':',color='black', alpha = 0.6,label='Deterministe')
     plt.ylabel(r'$|S_{12}|$',fontsize=16)
     plt.xlabel('f (GHz)',fontsize=16)
@@ -243,7 +250,6 @@ if __name__ == '__main__':
     plt.figure()
     plt.plot(freq_GHz, np.angle(S11_m),'-',color='red', label='PW - PCE')
     plt.plot(freq_GHz, np.angle(m_S11),'--',color='blue', alpha = 0.6, label='PW - MC [{0}]'.format(N_sample))
-    plt.fill_between(freq_GHz,np.angle(S11_m-1.96*std_S11),np.angle(S11_m+1.96*std_S11),color='red',alpha=0.3)
     plt.plot(freq_GHz,np.angle(lst_S11_tmp),':',color='black', alpha = 0.6,label='Deterministe')
     plt.ylabel(r'phase $S_{11}$', fontsize=16)
     plt.xlabel('f (GHz)', fontsize=16)
@@ -251,7 +257,6 @@ if __name__ == '__main__':
     plt.figure()
     plt.plot(freq_GHz, np.angle(S12_m),'-',color='red', label='PW - PCE')
     plt.plot(freq_GHz, np.angle(m_S12),'--',color='blue', alpha = 0.6, label='PW - MC [{0}]'.format(N_sample))
-    plt.fill_between(freq_GHz,np.angle(S12_m-1.96*std_S12),np.angle(S12_m+1.96*std_S12),color='red',alpha=0.3)
     plt.plot(freq_GHz,np.angle(lst_S12_tmp),':',color='black', alpha = 0.6,label='Deterministe')
     plt.ylabel(r'phase $S_{12}$', fontsize=16)
     plt.xlabel('f (GHz)', fontsize=16)

mainPCECP.py
@@ -1,5 +1,15 @@
 import time
+import matplotlib
+for backend in [ 'MACOSX','Qt5Agg', 'TkAgg', 'Agg']:
+    try:
+        matplotlib.use(backend, force=True)
+        import matplotlib.pyplot as plt
 
+        print(f"Using backend: {matplotlib.get_backend()}")
+        break
+    except Exception as e:
+        print(f"Failed to use backend {backend}: {e}")
+import matplotlib.pyplot as plt
 import numpy as np
 import chaospy as cp
 import math
@@ -13,41 +23,33 @@ def lognormal_params_from_mean_std(m, s):
     return mu, sigma
 #################### Parameters (without uncertainty) ###############################
 # Calculate the projection
-freq_GHz = np.linspace(220, 330, 101, endpoint=True)
+freq_GHz = np.linspace(220, 330, 201, endpoint=True)
 ##### Parameters for the three layers MUT (mat/mid/mar) - mut = material under test
 eps_mid = 1. - 0j
 
 
 ##################### Uncertainty over the parameters ###############################
 # Model the variation for the air gap (logN since positive definite)
-beta_1 = 30/100 # variation coefficient
+beta_1 = 20/100 # variation coefficient
 m1 = 50e-6  # mean
 sig1 = beta_1*m1 # standard-deviation
 m_c1,sig_c1 = lognormal_params_from_mean_std(m1,sig1)
 
-
-# Model the variation for the angle (N since can be positive or negative)
+# Model the variation of take-off angle (N since around 0)
 m2 = 0
 sig2 = 0.1
 
-
-# Model the variation for the thickness of the material thickness_mut = 0.00582
-beta_3 = 1/100 # variation coefficient
-m3 = 0.00582 # mean
-sig3 = beta_3*m3 # standard-deviation
+# Model the variation of the MUT thickness
+m3 = 0.00582
+beta_3 = 0.5/100
+sig3 = beta_3*m3
 m_c3,sig_c3 = lognormal_params_from_mean_std(m3,sig3)
 
-# # Model the variation for the eps of MUT eps_mut = 2. - 0.025j
-# beta_re =0/100
-# mre = 2
-# sigre = beta_re*mre
-# m_cre,sig_cre = lognormal_params_from_mean_std(mre,sigre)
-#
-# beta_im =0/100
-# mim = 0.025
-# sigim = beta_im*mim
-# m_cim,sig_cim = lognormal_params_from_mean_std(mim,sigim)
+
+# Other
 eps_mut = 2. - 0.025j
+eps_mid = 1. - 0j
+thickness_mut = m3
 
 
 # Use chaospy for the decomposition
@@ -55,29 +57,23 @@ t0 = time.perf_counter()
 X1 = cp.LogNormal(m_c1,sig_c1)
 X2 = cp.Normal(m2,sig2)
 X3 = cp.LogNormal(m_c3,sig_c3)
-# X4 = cp.LogNormal(m_cre,sig_cre)
-# X5 = cp.LogNormal(m_cim,sig_cim)
 
-# Joint distribution assuming independance
 X = cp.J(X1,X2,X3)
-
 # Decomposition using Wiener-Askey
-p = 6
-NbRV = 5
+p = 4
+NbRV = 3
 P = math.comb(p+NbRV,p)
 decomp = cp.generate_expansion(p,X)
-decomp = decomp.round(10)
 # Gauss quadrature adapted to our joint distribution
-d = 8
+d = 10
 tg,wg = cp.generate_quadrature(d,X,rule="gaussian")
 # Extract physical values
 x1_vals = tg[0, :]  # quadrature nodes for X1
-x2_vals = tg[1, :]  # quadrature nodes for X2
-x3_vals = tg[2, :]  # quadrature nodes for X3
-# x4_vals = tg[3, :]  # quadrature nodes for X4
-# x5_vals = tg[4, :]  # quadrature nodes for X5
+x2_vals = tg[1, :]
+x3_vals = tg[2, :]
 
-wg = wg[:, None]    # quadrature weigths
+
+wg = wg[:, None] # quadrature weigths
 
 # Normalization
 norms = np.array([cp.E(poly**2, X) for poly in decomp])
@@ -88,7 +84,7 @@ from utils.PlaneWave import SimPlaneWaveList # The blackbox code we use here for
 lst_S11_PC = []
 lst_S12_PC = []
 
-for x1, x2, x3 in tqdm(zip(x1_vals, x2_vals, x3_vals)):
+for x1,x2,x3 in tqdm(zip(x1_vals,x2_vals,x3_vals)):
     thickness_mid = x1
     angle_deg = x2
     thickness_mut = x3
@@ -119,21 +115,55 @@ for phi in decomp:
     coeffsS12.append(numers)
 
 coeffsS12 = np.array(coeffsS12)/norms[:, None]
+# After computing coeffsS11 and coeffsS12...
+
 t_PCE = time.perf_counter() - t0
 print('Time PCE : ',t_PCE)
+
+# Mean of complex S-parameters (zeroth order coefficient)
 S11_m = coeffsS11[0, :]
 S12_m = coeffsS12[0, :]
 
-S11_std = np.sum(coeffsS11[1:, :] ** 2, axis=-1)
-S12_std = np.sum(coeffsS12[1:, :] ** 2, axis=-1)
+# Compute E[|S|²] directly from coefficients
+# For complex S = Σ s_i Ψ_i, we have |S|² = Σ |s_i|² (by orthogonality)
+E_abs_S11_squared = np.sum(np.abs(coeffsS11)**2 * norms[:, None], axis=0)
+E_abs_S12_squared = np.sum(np.abs(coeffsS12)**2 * norms[:, None], axis=0)
+E_angle_S11_squared = np.sum(np.angle(coeffsS11)**2 * norms[:, None], axis=0)
+E_angle_S12_squared = np.sum(np.angle(coeffsS12)**2 * norms[:, None], axis=0)
+# Compute E[|S|] using quadrature
+# Evaluate |S| at quadrature points and integrate
+S11_at_quad = np.zeros((len(x1_vals), len(freq_GHz)), dtype=complex)
+S12_at_quad = np.zeros((len(x1_vals), len(freq_GHz)), dtype=complex)
+
+for i, phi in enumerate(decomp):
+    phi_vals = phi(*tg)[:, None]  # shape (n_quad_points, 1)
+    S11_at_quad += coeffsS11[i, :] * phi_vals
+    S12_at_quad += coeffsS12[i, :] * phi_vals
+
+
+
+# Compute E[|S|] using quadrature
+E_abs_S11 = np.sum(wg * np.abs(S11_at_quad), axis=0)
+E_abs_S12 = np.sum(wg * np.abs(S12_at_quad), axis=0)
+E_angle_S11 = np.sum(wg * np.angle(S11_at_quad), axis=0)
+E_angle_S12 = np.sum(wg * np.angle(S12_at_quad), axis=0)
+
+# Variance of magnitude: Var(|S|) = E[|S|²] - E[|S|]²
+S11_std = E_abs_S11_squared - E_abs_S11**2
+S12_std = E_abs_S12_squared - E_abs_S12**2
+S11_ang_std = E_angle_S11_squared - E_angle_S11**2
+S12_ang_std = E_angle_S12_squared - E_angle_S12**2
+
+# Mean of magnitude (for plotting)
+S11_m_mag = E_abs_S11
+S12_m_mag = E_abs_S12
 
 ######################## MONTE-CARLO ##########################################
 t0 = time.perf_counter()
 N_sample = 100000
-# Create the samples for the different variables
-thickness_mid_samples = np.random.lognormal(m_c1,sig_c1,int(N_sample))
-angle_deg_samples = np.random.normal(m2,sig2,int(N_sample))
-thickness_mut_samples = np.random.lognormal(m_c3,sig_c3,int(N_sample))
+thickness_mid_samples = np.random.lognormal(m_c1, sig_c1, int(N_sample))
+angle_deg_samples = np.random.normal(m2, sig2, int(N_sample))
+thickness_mut_samples = np.random.lognormal(m_c3, sig_c3, int(N_sample))
 
 lst_S11 = []
 lst_S12 = []
@@ -142,8 +172,8 @@ for i in tqdm(range(N_sample)):
     lst_S11_tmp = []
     lst_S12_tmp = []
     for f_GHz in freq_GHz:
-        # Plane wave model
-        S11, S12, _, _ = SimPlaneWaveList(f_GHz, angle_deg_samples[i], eps_mut, thickness_mut_samples[i], eps_mid, thickness_mid_samples[i])
+        S11, S12, _, _ = SimPlaneWaveList(f_GHz, angle_deg_samples[i], eps_mut, thickness_mut_samples[i],
+                                          eps_mid, thickness_mid_samples[i])
         lst_S11_tmp.append(S11)
         lst_S12_tmp.append(S12)
     lst_S11.append(np.array(lst_S11_tmp))
@@ -151,60 +181,76 @@ for i in tqdm(range(N_sample)):
 
 lst_S11 = np.array(lst_S11)
 lst_S12 = np.array(lst_S12)
-t_MC = time.perf_counter()-t0
-print('Time MC : ',t_MC)
-print('Gain in time : ', t_MC/t_PCE)
+t_MC = time.perf_counter() - t0
+print('Time MC : ', t_MC)
+print('Gain in time : ', t_MC / t_PCE)
 
-m_S11 = np.sum(lst_S11,axis=0)/N_sample
-m_S12 = np.sum(lst_S12,axis=0)/N_sample
-std_S11 = (np.sum(lst_S11**2-m_S11**2,axis=0))/(N_sample-1)
-std_S12 = (np.sum(lst_S12**2-m_S12**2,axis=0))/(N_sample-1)
+m_S11 = np.mean(np.abs(lst_S11), axis=0)
+m_S12 = np.mean(np.abs(lst_S12), axis=0)
+std_S11 = np.var(np.abs(lst_S11), axis=0, ddof=1)
+std_S12 = np.var(np.abs(lst_S12), axis=0, ddof=1)
 
 ############################### DETERMINIST CASE #############################################
 dS11 = []
 dS12 = []
 for f_GHz in freq_GHz:
-    S11, S12, _, _ = SimPlaneWaveList(f_GHz, m2, eps_mut, m3, eps_mid, m1)
+    S11, S12, _, _ = SimPlaneWaveList(f_GHz, m2, eps_mut, thickness_mut, eps_mid, m1)
     dS11.append(S11)
     dS12.append(S12)
 
 ######################## PLOT THE RESULTS #####################################
-
-import matplotlib
-matplotlib.use("macOSX")
-import matplotlib.pyplot as plt
-
 plt.figure()
-plt.plot(freq_GHz,np.abs(S11_m),'-',color='red',label='PCE')
-plt.plot(freq_GHz,np.abs(m_S11),'--',color='green',label='MC')
-plt.plot(freq_GHz,np.abs(dS11),'--',color='black',label='Det')
-
+plt.plot(freq_GHz, S11_m_mag, '-', color='red', label='PCE')
+plt.plot(freq_GHz, m_S11, '--', color='green', label='MC')
+plt.plot(freq_GHz, np.abs(dS11), '--', color='black', label='Det')
+plt.fill_between(freq_GHz,
+                 S11_m_mag - 1.64 * np.sqrt(S11_std),
+                 S11_m_mag + 1.64 * np.sqrt(S11_std),
+                 color='red', alpha=0.3)
+plt.ylabel(r'$|S_{11}|$', fontsize=16)
+plt.xlabel('f (GHz)', fontsize=16)
 plt.legend()
+
 plt.figure()
-plt.plot(freq_GHz,np.abs(S12_m),'-',color='red',label='PW - PCE')
-plt.plot(freq_GHz,np.abs(m_S12),'--',color='green',label='MC')
-plt.plot(freq_GHz,np.abs(dS12),'--',color='black',label='Det')
-plt.ylabel(r'$|S_{12}|$',fontsize=16)
-plt.xlabel('f (GHz)',fontsize=16)
+plt.plot(freq_GHz, S12_m_mag, '-', color='red', label='PW - PCE')
+plt.plot(freq_GHz, m_S12, '--', color='green', label='MC')
+plt.plot(freq_GHz, np.abs(dS12), '--', color='black', label='Det')
+plt.fill_between(freq_GHz,
+                 S12_m_mag - 1.64 * np.sqrt(np.abs(S12_std)),
+                 S12_m_mag + 1.64 * np.sqrt(np.abs(S12_std)),
+                 color='red', alpha=0.3)
+plt.ylabel(r'$|S_{12}|$', fontsize=16)
+plt.xlabel('f (GHz)', fontsize=16)
 plt.legend()
+
 plt.figure()
-plt.plot(freq_GHz, np.angle(S11_m),'-',color='red', label='PCE')
-plt.plot(freq_GHz,np.angle(m_S11),'--',color='green',label='MC')
-plt.plot(freq_GHz,np.angle(dS11),'--',color='black',label='Det')
+plt.plot(freq_GHz, np.angle(S11_m), '-', color='red', label='PCE')
+# plt.fill_between(freq_GHz,
+#                  E_angle_S11 - 1.64 * np.sqrt(S11_ang_std),
+#                  E_angle_S11 + 1.64 * np.sqrt(S11_ang_std),
+#                  color='red', alpha=0.3)
+plt.plot(freq_GHz, np.angle(np.mean(lst_S11, axis=0)), '--', color='green', label='MC')
+plt.plot(freq_GHz, np.angle(dS11), '--', color='black', label='Det')
 plt.ylabel(r'phase $S_{11}$', fontsize=16)
 plt.xlabel('f (GHz)', fontsize=16)
 plt.legend()
+
 plt.figure()
-plt.plot(freq_GHz, np.angle(S12_m),'-',color='red', label='PCE')
-plt.plot(freq_GHz,np.angle(m_S12),'--',color='green',label='MC')
-plt.plot(freq_GHz,np.angle(dS12),'--',color='black',label='Det')
+plt.plot(freq_GHz, np.angle(S12_m), '-', color='red', label='PCE')
+plt.plot(freq_GHz, np.angle(np.mean(lst_S12, axis=0)), '--', color='green', label='MC')
+# plt.fill_between(freq_GHz,
+#                  E_angle_S12 - 1.64 * np.sqrt(S12_ang_std),
+#                  E_angle_S12 + 1.64 * np.sqrt(S12_ang_std),
+#                  color='red', alpha=0.3)
+plt.plot(freq_GHz, np.angle(dS12), '--', color='black', label='Det')
 plt.ylabel(r'phase $S_{12}$', fontsize=16)
 plt.xlabel('f (GHz)', fontsize=16)
 plt.legend()
 plt.show()
 
-print('err S11 l2 : ', np.linalg.norm(S11_m - m_S11) / np.linalg.norm(m_S11))
-print('err S12 l2 : ', np.linalg.norm(S12_m - m_S12) / np.linalg.norm(m_S12))
-print(len(std_S11),len(S11_std))
-print('err S11 l2 [m2] : ', np.linalg.norm(std_S11 - S11_std))
-print('err S12 l2 [m2] : ', np.linalg.norm(std_S12 - S12_std))
+print('err S11 mean l2 : ', np.linalg.norm(S11_m_mag - m_S11) / np.linalg.norm(m_S11))
+print('err S12 mean l2 : ', np.linalg.norm(S12_m_mag - m_S12) / np.linalg.norm(m_S12))
+print('err S11 var l2 : ', np.linalg.norm(std_S11 - S11_std) / np.linalg.norm(std_S11))
+print('err S12 var l2 : ', np.linalg.norm(std_S12 - S12_std) / np.linalg.norm(std_S12))
+print('Variance S11 [0] - MC vs PCE:', std_S11[0], S11_std[0])
+print('Variance S12 [0] - MC vs PCE:', std_S12[0], S12_std[0])